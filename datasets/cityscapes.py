Fix depth normalization for cityscapes

datasets/cityscapes.py
@@ -63,7 +63,9 @@ class CityscapesSegmentation(data.Dataset):
 
         _img = Image.open(img_path).convert('RGB')
         if self.use_depth:
-            _depth = Image.open(depth_path).convert('L')
+            _depth_arr = np.asarray(Image.open(depth_path), dtype='float')
+            # _depth_arr /= 25000 * 256 # Empirically determined normalization value (2.5 std)
+            _depth = Image.fromarray(_depth_arr / 25000 * 256).convert('L')
             _img.putalpha(_depth)
 
         _tmp = np.array(Image.open(lbl_path), dtype=np.uint8)

utils.py
@@ -68,6 +68,7 @@ def encode_segmap(mask):
     label_mask = label_mask.astype(int)
     return label_mask
 
+
 def sample_distribution(dataset):
     n = min(1000, len(dataset))
     m = 100
@@ -85,7 +86,16 @@ def sample_distribution(dataset):
 
     mean = np.mean(samples, axis=0)
     std = np.std(samples, axis=0)
-    return {'mean': mean, 'std': std}
+    m_max = np.max(samples, axis=0)
+
+    if dataset.use_depth:
+        import matplotlib.pyplot as plt
+        plt.hist(samples[:, -1], bins='auto')
+        plt.title("Depth histogram")
+        plt.show()
+
+    return {'mean': mean, 'std': std, 'max': m_max}
+
 
 def get_cityscapes_labels():
     return np.array([