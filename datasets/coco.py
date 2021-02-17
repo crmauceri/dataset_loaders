Fixed bbox calculation

datasets/coco.py
@@ -143,13 +143,13 @@ class COCOSegmentationSampleLoader(SampleLoader):
             _target = np.zeros((len(cocotarget), 5))
             for i, x in enumerate(cocotarget):
                 # [label, center_x/w, center_y/h, bbox_w/w, bbox_h/h]
-                center_x = (x['bbox'][1] + x['bbox'][3]/2.0)/img_metadata['width']
-                center_y = (x['bbox'][0] + x['bbox'][2]/2.0)/img_metadata['height']
+                center_x = (x['bbox'][0] + x['bbox'][2]/2.0)/img_metadata['width']
+                center_y = (x['bbox'][1] + x['bbox'][3]/2.0)/img_metadata['height']
                 w = x['bbox'][3]/img_metadata['width']
                 h = x['bbox'][2]/img_metadata['height']
 
                 _target[i, 0] = x['category_id']
-                _target[i, 1:] = [center_y, center_x, h, w]
+                _target[i, 1:] = [center_x, center_y, w, h]
         else:
             raise ValueError("DATASET.ANNOTATION_TYPE={} not implemented".format(self.cfg.DATASET.ANNOTATION_TYPE))
 